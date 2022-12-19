6993c441195c32f58e1624889d07d08e0e580a3f
Simon Johansson <99795909+Simxn999@users.noreply.github.com>
2022-12-19 19:53:53 +0100

    10/12/2022

--- 10_A/Program.cs
+++ 10_A/Program.cs
@@ -4,7 +4,34 @@ var file = File.ReadAllLines(filePath);
 
 //
 
+var cycles = new List<int>{ 1 };
 
+foreach (var line in file)
+{
+    var input = line.Split(' ');
+
+	switch (input)
+	{
+		case ["addx", string x]:
+			for (int i = 0; i < 2; i++)
+			{
+				cycles.Add(i == 1 ? cycles.Last() + int.Parse(x) : cycles.Last());
+			}
+			break;
+
+		case ["noop"]:
+			cycles.Add(cycles.Last());
+			break;
+
+		default:
+			throw new Exception("que?");
+	}
+}
+
+for (int i = 20; i <= 220; i += 40)
+{
+	output += i * cycles[i - 1];
+}
 
 //
 
--- 10_B/Program.cs
+++ 10_B/Program.cs
@@ -4,11 +4,50 @@ var file = File.ReadAllLines(filePath);
 
 //
 
+var _x = 1;
+var cycles = new List<string>();
 
+foreach (var line in file)
+{
+    var input = line.Split(' ');
+
+    switch (input)
+    {
+        case ["addx", string x]:
+            for (int i = 0; i < 2; i++)
+            {
+                cycles.Add(IsInSprite(cycles.Count) ? "##" : "  ");
+
+                if (i == 1)
+                    _x += int.Parse(x);
+            }
+            break;
+
+        case ["noop"]:
+            cycles.Add(IsInSprite(cycles.Count) ? "##" : "  ");
+            break;
+
+        default:
+            throw new Exception("que?");
+    }
+}
+
+for (int i = 1; i <= cycles.Count; i++)
+{
+    Console.Write(cycles[i - 1]);
+
+    if (i % 40 == 0)
+        Console.WriteLine();
+}
 
 //
 
 Console.WriteLine();
-Console.WriteLine($"""Answer = "{output}" """);
 Console.WriteLine("Press [Q] to exit...");
 while (Console.ReadKey(true).Key != ConsoleKey.Q) { }
+
+bool IsInSprite(int pixel)
+{
+    pixel -= pixel / 40 * 40;
+    return pixel >= _x - 1 && pixel <= _x + 1;
+}
